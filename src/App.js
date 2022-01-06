e8d614f19a3d885d6fb04931f8867fdcdb4aebba
Drewshka <andrew.jos.stevenson@gmail.com>
2022-01-06 14:22:16 -0500

    finished step one

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import axios from 'axios';
+class App extends React.Component {
+        state = {
+            students: [],
+            DataisLoaded: false
+        };
+   
+        componentDidMount() {
+          axios.get("https://api.hatchways.io/assessment/students").then((response) => {
+            console.log("Students: ", response);
+            this.setState({ students: response.data.students });
+          });
+        }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    render() { 
+      console.log(this.state.students);
+  
+   return (
+    <div className="app">
+        <h2>Students</h2>
+        <article>
+          {this.state.students.map((student) => {
+
+            function findAverage(array) {
+              let sum = 0;
+              for (let i = 0; i < array.length; i++) {
+                sum += parseInt(array[i]);
+              }
+              let average = sum / array.length;
+              return average;
+            }
+            
+            const averageGrade = findAverage(student.grades);
+
+            return (
+              <div key={student.id}>
+              <img src={student.pic} alt="avatar" />
+              <h3>{student.firstName} {student.lastName}</h3>
+              <p>Email: {student.email}</p>
+              <p>Company: {student.company}</p>
+              <p>Skill: {student.skill}</p>
+              <p>Average: {averageGrade}</p>
+                                                        
+            </div>
+            )
+          })}
+        </article>
+      </div>
+    );
+  }
 }
-
+   
 export default App;
